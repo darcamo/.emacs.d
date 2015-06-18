7326d236fdde249c24f8312336e2064f96cdf779
Darlan Cavalcante Moreira <darcamo@gmail.com>
2015-06-18 18:02:58 -0300

    Remove marmalade repository from package-archives
    Most packages are in other repos and the marmalade certificate expired.

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
 ;; xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 (setq package-enable-at-startup nil)
 (with-eval-after-load "package"
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
