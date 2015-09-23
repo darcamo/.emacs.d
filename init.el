b18254df083c0d4e820f4bb3695e21c09900432b
Darlan Cavalcante Moreira <darcamo@gmail.com>
2015-09-23 13:24:14 -0300

    Comment elpy package archive addition
    It was not responding and making package reloading much slower

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
   ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+  ;; (add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
   ;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   )
 (package-initialize)
