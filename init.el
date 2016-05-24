7ee8bb5d52ff28d39aad8d2f191652c7c222b199
Darlan Cavalcante Moreira <darcamo@gmail.com>
2016-05-24 14:31:12 -0300

    Update package list and elpa address

--- init.el
+++ init.el
@@ -39,11 +39,12 @@
  '(elpy-test-runner (quote elpy-test-nose-runner))
  '(package-selected-packages
    (quote
-    (ace-mc csv-mode geeknote helm-ag anaconda-mode pdf-tools esup which-key highlight-numbers jdee helm-unicode firestarter origami java-snippets helm-pages page-break-lines ob-ipython use-package web-beautify json-reformat company-edbi edbi edbi-sqlite rainbow-delimiters lorem-ipsum sqlup-mode sql-indent company-tern impatient-mode skewer-mode js2-mode web-mode expand-region helm-projectile projectile emacs-eclim indent-guide avy ein helm highlight-indentation jedi magit markdown-mode multiple-cursors pyvenv helm-bibtex writegood-mode helm-company helm-gtags ggtags company-c-headers smart-mode-line shut-up epl git commander f dash s gscholar-bibtex base16-theme irony-eldoc flycheck-irony hydra ace-jump-mode auctex company dired-details flycheck irony yasnippet company-irony image-dired+ helm-swoop yaml-mode w3m virtualenvwrapper vc-check-status unfill undo-tree ucs-utils thesaurus tabulated-list string-utils ssh-config-mode regex-tool rebox2 realgud rainbow-mode pos-tip paradox oauth2 nose nlinum naquadah-theme multi move-text math-symbol-lists makefile-runner magit-popup magit-filenotify jinja2-mode imenu+ iedit highlight-symbol hideshowvis goto-last-change gnuplot gitignore-mode gitconfig-mode git-timemachine git-gutter fringe-helper elpy ecb ebib discover-my-major dired-details+ diminish cython-mode cppcheck company-inf-python company-auctex comment-dwim-2 cmake-mode button-lock buffer-move bookmark+ bbyac badger-theme anzu anaphora ace-window ace-isearch ac-dabbrev)))
+    (company-irony-c-headers pdf-tools auto-virtualenv helm-org-rifle ace-mc csv-mode anaconda-mode esup which-key highlight-numbers jdee helm-unicode firestarter origami java-snippets helm-pages page-break-lines ob-ipython use-package web-beautify json-reformat company-edbi edbi edbi-sqlite rainbow-delimiters lorem-ipsum sqlup-mode sql-indent company-tern impatient-mode skewer-mode js2-mode web-mode expand-region helm-projectile projectile emacs-eclim indent-guide avy ein helm highlight-indentation jedi magit markdown-mode multiple-cursors pyvenv helm-bibtex writegood-mode helm-company helm-gtags ggtags smart-mode-line shut-up epl git commander f dash s gscholar-bibtex base16-theme irony-eldoc flycheck-irony hydra ace-jump-mode auctex company dired-details flycheck irony company-irony image-dired+ helm-swoop yaml-mode w3m virtualenvwrapper vc-check-status unfill undo-tree ucs-utils thesaurus tabulated-list string-utils ssh-config-mode regex-tool rebox2 realgud rainbow-mode pos-tip paradox oauth2 nose nlinum naquadah-theme multi move-text math-symbol-lists makefile-runner magit-popup magit-filenotify jinja2-mode imenu+ iedit highlight-symbol hideshowvis goto-last-change gnuplot gitignore-mode gitconfig-mode git-timemachine git-gutter fringe-helper elpy ecb ebib discover-my-major dired-details+ diminish cython-mode cppcheck company-inf-python company-auctex comment-dwim-2 cmake-mode button-lock buffer-move bookmark+ bbyac badger-theme anzu anaphora ace-window ace-isearch ac-dabbrev)))
  '(paradox-automatically-star t)
  '(safe-local-variable-values
    (quote
-    ((pyvenv-workon . pyphysim)
+    ((abbrev-file-name . "customabbrev_defs")
+     (pyvenv-workon . pyphysim)
      (eval read-abbrev-file
            (fullpath-relative-to-current-file "customabbrev_defs"))
      (engine . jinja)
@@ -89,7 +90,7 @@
 (setq package-enable-at-startup nil)
 (with-eval-after-load "package"
   ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  ;; (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   ;; (add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
   ;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
